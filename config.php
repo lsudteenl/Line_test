fccde9e3592f022af364ee2f1182080659b5a0d8
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-07-20 22:47:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,15 +4,15 @@ $line_AccessToken = "S68JfNI9j66QapyL7oIn3Ttv/yJf6LOY2WcdQaVPJz/bK2r63YegJIl7UfS
 $GLOBALS['linenotify_token'] = array("8mdAiI5Vpmo6AzBd6og0jrXEAKCvBVzXyVuSQjEsY8M"); //สามารถ Gen ได้จาก https://notify-bot.line.me/my/ 1 Token ต่อ 1 กลุ่ม
 
 //Data Base
-$dbhost = "localhost";
-$dbname = "opdevmen_eticket";
-$dbusername = "opdevmen_eticket";
-$dbpassword = "line#eticket";
+$dbhost = "";
+$dbname = "";
+$dbusername = "";
+$dbpassword = "";
 
 // Mail
-$to = 'koopybz@gmail.com';
-$from = 'support@opdev.men';
-$from_name = 'OPDEV Support Team';
+$to = 'ruttanon.n@carabao.co.th';
+$from = 'carabaocothitsupport@cbd.freshservice.com';
+$from_name = 'App Support Team';
 
 
 // อย่าลืมตั้ง 777 ให้กับโฟลเดอร์ content ไม่งั้นจะลบไฟล์ไม่ได้
