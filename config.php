1b30be1673fe930605494743753fa9c79c0ad874
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-07-20 22:24:56 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// Line Access token
+$line_AccessToken = "S68JfNI9j66QapyL7oIn3Ttv/yJf6LOY2WcdQaVPJz/bK2r63YegJIl7UfSWK6UGxBC8iv7xtfFVG545I3JdWXFlJ+tWAgreHmt1YfCjcO1wjRU5OaDRcNz2HWAIbJm9hWVFU6NAUiJg+gZxPkzaeAdB04t89/1O/w1cDnyilFU="; //Line Developer Token
+$GLOBALS['linenotify_token'] = array("5G3XQ4iJNR7ToY4NrkLwjXwpgk0OkDmeQekmWGvbOZx"); //สามารถ Gen ได้จาก https://notify-bot.line.me/my/ 1 Token ต่อ 1 กลุ่ม
+
+//Data Base
+$dbhost = "";
+$dbname = "";
+$dbusername = "";
+$dbpassword = "";
+
+// Mail
+$to = 'ruttanon.n@carabao.co.th';
+$from = 'carabaocothitsupport@cbd.freshservice.com';
+$from_name = 'OPDEV Support Team';
+
+
+// อย่าลืมตั้ง 777 ให้กับโฟลเดอร์ content ไม่งั้นจะลบไฟล์ไม่ได้
+
+?>
