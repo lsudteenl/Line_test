97b0b2d3ddcaed6044e780fece61e09fa020338f
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-07-20 22:46:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Line Access token
-$line_AccessToken = "d0jyEwZYG7bHT6J2jSVaOcdN3gxEesdbq1pkDdNvH/vltEVfS8v0K/9wC3bGOPXtHq+KzS1nDWnJVAgpE2eH7m9i0bIKr8c6d1YUwukypR93vKSEFwZEVCfWgKkfPN3KC50OCriXb2vVMzs4a6ZbHwdB04t89/1O/w1cDnyilFU="; //Line Developer Token
-$GLOBALS['linenotify_token'] = array("spHphqn5t4I9T5G11Dt9Pb9Q9YRsxmULNM96YKDrRwE"); //สามารถ Gen ได้จาก https://notify-bot.line.me/my/ 1 Token ต่อ 1 กลุ่ม
+$line_AccessToken = "S68JfNI9j66QapyL7oIn3Ttv/yJf6LOY2WcdQaVPJz/bK2r63YegJIl7UfSWK6UGxBC8iv7xtfFVG545I3JdWXFlJ+tWAgreHmt1YfCjcO1wjRU5OaDRcNz2HWAIbJm9hWVFU6NAUiJg+gZxPkzaeAdB04t89/1O/w1cDnyilFU=; //Line Developer Token
+$GLOBALS['linenotify_token'] = array("8mdAiI5Vpmo6AzBd6og0jrXEAKCvBVzXyVuSQjEsY8M"); //สามารถ Gen ได้จาก https://notify-bot.line.me/my/ 1 Token ต่อ 1 กลุ่ม
 
 //Data Base
 $dbhost = "localhost";
